9e9153a08174e5e8843d0160b549229ca97047eb
Hung Nguyen <hungnt89@gmail.com>
2020-06-21 15:10:45 +0700

    Navbar now can sticky.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const navBar = document.querySelector("#nav");
 const toggleBtn = document.getElementById("nav-toggle");
 const navLinks = document.querySelector(".nav-links");
 
@@ -5,4 +6,14 @@ const explore = document.querySelector(".explore");
 
 toggleBtn.addEventListener("click", () => {
   navLinks.classList.toggle("show");
+});
+
+const navBarHeight = navBar.getBoundingClientRect().height;
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > navBarHeight) {
+    navBar.classList.add("sticky");
+  } else {
+    navBar.classList.remove("sticky");
+  }
 });
